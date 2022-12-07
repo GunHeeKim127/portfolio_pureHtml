7605713490b3261eb9839151e7c9d9be8c879b1a
R.Z.A <53036090+GunHeeKim127@users.noreply.github.com>
2022-12-08 00:47:42 +0900

    12.07 오늘의 마지막 업데이트

---
+++ public/javascripts/project.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    document.getElementById("project").innerHTML
+    ='<object type="text/html" data="../view/project.html" style="height:625px; width:100%;"></object>';
+    document.getElementById("project").style.height="625px"
+})
