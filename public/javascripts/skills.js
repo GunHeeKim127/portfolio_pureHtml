b0c758945316f89179a96ba41c6f0d5cf463d5af
R.Z.A <53036090+GunHeeKim127@users.noreply.github.com>
2022-12-07 18:32:04 +0900

    12.07 ejs로 만든곳까지 복원

--- public/javascripts/skills.js
+++ public/javascripts/skills.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     document.getElementById("skills").innerHTML
-    ='<object type="text/html" data="../view/skills.html" style="height:60px; width:100%;"></object>';
+    ='<object type="text/html" data="../view/skills.html" style="height:625px; width:100%;"></object>';
+    document.getElementById("skills").style.height="625px"
 })
