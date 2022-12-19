01c8f33e1b5d2a72b469e9710a1fcb437118d132
R.Z.A <53036090+GunHeeKim127@users.noreply.github.com>
2022-12-19 23:12:33 +0900

    22.12.19 업데이트

--- public/javascripts/career.js
+++ public/javascripts/career.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     document.getElementById("career").innerHTML
     ='<object type="text/html" data="../view/career.html" style="width:100%; height:100%;"></object>';
-    document.getElementById("career").style.height="1220px"
+    document.getElementById("career").style.height="1350px"
 })
